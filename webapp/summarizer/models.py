baa09c4636d85f6f7372ec1fc60b26b54045c2c6
nelson.spencerg@gmail.com <138039144+SpencerGNelson@users.noreply.github.com>
2026-04-20 13:48:53 -0600

    Adding new Summary model and migration

--- webapp/summarizer/models.py
+++ webapp/summarizer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Summary(models.Model):
+    company_name = models.CharField(max_length=225)
+    url = models.CharField(max_length=500)
+    data_collected = models.TextField()
+    third_party_sharing = models.TextField()
+    your_rights = models.TextField()
+    cancellation_refund_policy = models.TextField()
+    red_flags = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
